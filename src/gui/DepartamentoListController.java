7a7e654b899b96e956d9bbad4d7c9392bbc5b191
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-08-28 10:46:22 -0300

    Criação da tela de cadastro do departamento

--- src/gui/DepartamentoListController.java
+++ src/gui/DepartamentoListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alertas;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Departamento;
 import model.service.DepartamentoService;
@@ -31,9 +40,10 @@ public class DepartamentoListController implements Initializable
 	private Button btNew;
 	private ObservableList<Departamento> obsList;
 	@FXML
-	public void onBtAction()
+	public void onBtAction(ActionEvent event)
 	{
-		System.out.println("Click");
+		Stage parentStage=Utils.currentStage(event);
+		createDialogForm("/gui/DepartamentoForm.fxml",parentStage );
 	}
 	@Override
 	public void initialize(URL url, ResourceBundle rb) 
@@ -60,5 +70,24 @@ public class DepartamentoListController implements Initializable
 		List<Departamento> departamentos=ds.findAll();
 		obsList=FXCollections.observableArrayList(departamentos);
 		tableViewDepartamento.setItems(obsList);
+	}
+	private void createDialogForm(String absoluteName,Stage parentStage)
+	{
+		try
+		{
+			FXMLLoader loader=new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane=loader.load();
+			Stage dialogStage=new Stage();
+			dialogStage.setTitle("Cadastro de departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e)
+		{
+			Alertas.showAlert("IO Exception", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }	
